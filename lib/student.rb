1c50c30e62a33f5a49367dc9ae0f2a3367532f6d
Alison Williams <apw5011@gmail.com>
2021-01-16 21:52:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,8 +26,9 @@ class Student
   
   def self.drop_table 
     sql = <<-SQL
-      DROP TABLE IF NOT EXISTS 
+      "DROP TABLE IF EXISTS students" 
     SQL
-
+    
+    DB[:conn].execute(sql)
 
 end
