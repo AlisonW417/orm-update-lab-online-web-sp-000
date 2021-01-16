268a5455b6c1a1ecf11c1594e1078e50dcc48fe6
Alison Williams <apw5011@gmail.com>
2021-01-16 22:18:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,11 +66,12 @@ class Student
       SELECT *
       FROM students 
       WHERE name = ? 
+      LIMIT 1
     SQL
     
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
-    end 
+    end.first 
   end 
 
 end
