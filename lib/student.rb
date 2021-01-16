7c73555fb79047cf8fdb3d71303a1dbaa2ec5fb9
Alison Williams <apw5011@gmail.com>
2021-01-16 22:04:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,7 +55,12 @@ class Student
     new_student 
   end 
   
-  def self.new_from_db(row) 
+  def self.new_from_db(row)
+    new_student = self.new(id, name, grade)
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2] 
+    new_student
   end 
 
 end
